b1978ba8ec13a9bece8b10c3b5c2c18e445238c5
Amirmasoud <amirmasood33@gmail.com>
2020-11-19 22:06:37 +0330

    Remove husky pre-commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,43 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">amirmasoud</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="flex flex-wrap">
+    <div
+      class="transition duration-500 ease-in-out w-1/5 p-4 h-screen transform hover:scale-90 hover:-skew-y-12 bg-gradient-to-r from-blue-500 to-blue-600 flex flex-col text-center justify-center items-center"
+    >
+      <h1 class="w-full font-blog text-6xl text-blue-900">BLOG</h1>
+      <p class="w-full pt-4 text-sm italic text-blue-900">
+        Wants to read some badly read articles?
+      </p>
+    </div>
+    <div
+      class="transition duration-500 ease-in-out w-1/5 p-4 h-screen transform hover:scale-90 hover:-skew-y-6 bg-gradient-to-r from-pink-500 to-pink-600 flex flex-col text-center justify-center items-center"
+    >
+      <h1 class="font-project text-6xl text-pink-900">PROJECTS</h1>
+      <p class="w-full pt-4 text-sm italic text-pink-900">
+        Wants to see some badly implemented projects?
+      </p>
+    </div>
+    <div
+      class="transition duration-500 ease-in-out w-1/5 p-4 h-screen transform hover:scale-95 bg-gradient-to-r from-green-500 to-green-600 flex flex-col text-center justify-center items-center"
+    >
+      <h1 class="font-about text-6xl text-green-900">ABOUT</h1>
+      <p class="w-full pt-4 text-sm italic text-green-900">Who the f*?</p>
+    </div>
+    <div
+      class="transition duration-500 ease-in-out w-1/5 p-4 h-screen transform hover:scale-90 hover:skew-y-6 bg-gradient-to-r from-indigo-500 to-indigo-600 flex flex-col text-center justify-center items-center"
+    >
+      <h1 class="font-photo text-6xl text-indigo-900">PHOTOS</h1>
+      <p class="w-full pt-4 text-sm italic text-indigo-900">
+        Some serious, genuine amature photos
+      </p>
+    </div>
+    <div
+      class="transition duration-500 ease-in-out w-1/5 p-4 h-screen transform hover:scale-90 hover:skew-y-12 bg-gradient-to-r from-purple-500 to-purple-600 flex flex-col text-center justify-center items-center"
+    >
+      <h1 class="font-drawing text-6xl text-purple-900">DRAWING</h1>
+      <p class="w-full pt-4 text-sm italic text-purple-900">
+        Some terribly drawn photos by a guy who thinks can be artist withing a
+        day, yay!
+      </p>
     </div>
   </div>
 </template>
@@ -28,41 +45,3 @@
 <script>
 export default {}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans', ...defaultTheme.fontFamily.sans],
+        blog: ['Bangers', 'cursive'],
+        photo: ['Bungee Shade', 'cursive'],
+        drawing: ['Rock Salt', 'cursive'],
+        project: ['Lily Script One', 'cursive'],
+        about: ['Oleo Script', 'cursive'],
+      },
+    },
+  },
+}
