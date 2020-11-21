4ca154c8213c29a3bec8987a2199f5e2486b8b7d
Amirmasoud <amirmasood33@gmail.com>
2020-11-21 22:21:34 +0330

    Blog

--- pages/index.vue
+++ pages/index.vue
@@ -41,13 +41,43 @@
         </p>
       </div>
     </div>
-    <div class="container mx-auto relative h-screen">
-      <span
-        class="absolute h-32 w-32 bg-gradient-to-r from-blue-300 via-blue-400 to-blue-500 top-0 mt-4 -ml-16 rounded-full z-0 shadow-lg"
-      ></span>
-      <h1 class="absolute w-full pt-16 font-blog text-6xl text-blue-900 z-10">
-        BLOG
-      </h1>
+    <div class="bg-gray-900">
+      <div class="container mx-auto relative h-screen">
+        <span
+          class="absolute h-64 w-64 bg-gradient-to-r from-blue-700 via-blue-800 to-blue-900 top-0 mt-12 ml-20 rounded-full z-0 shadow-lg"
+        ></span>
+        <h1 class="absolute w-full pt-16 font-blog text-6xl text-blue-400 z-10">
+          BLOG
+        </h1>
+        <div class="relative flex flex-wrap pt-48 z-20">
+          <div v-for="index in [1, 2, 3]" :key="index" class="w-1/3 p-4">
+            <div
+              class="bg-gradient-to-b from-gray-800 to-gray-900 rounded-xl shadow-lg"
+            >
+              <div
+                class="bg-cover bg-center w-full h-64 rounded-xl transform scale-105 shadow-lg"
+                style="
+                  background-image: url(https://images.unsplash.com/photo-1605940169841-60884072a854?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60);
+                "
+              ></div>
+              <div class="p-4">
+                <p class="font-sans text-sm font-bold text-gray-700">
+                  Yesterday &bull; #fun #fancy
+                </p>
+                <h2 class="font-sans pb-4 text-2xl font-bold text-gray-500">
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit
+                </h2>
+                <p class="font-sans text-gray-600">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Commodi nulla minima consequuntur temporibus, aliquid delectus
+                  vitae sapiente omnis laboriosam accusantium dolorem mollitia
+                  architecto fugiat quas adipisci nam fuga quam maiores?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
