22fe0fc9e63c6e4e13220d4dbbc58881233bfba5
Amirmasoud Sheydaei <amirmasood33@gmail.com>
2021-02-06 23:53:56 +0330

    Add image to books

--- pages/index.vue
+++ pages/index.vue
@@ -80,10 +80,20 @@
     <section id="books" class="container mx-auto my-64 px-4 sm:px-16">
       <h1 class="text-gray-900 font-bold text-3xl">Books</h1>
       <span class="text-blue-500">______</span>
-      <div class="my-16 flex flex-wrap">
-        <div class="uppercase w-1/5">
-          <h2 class="font-bold text-sm">A sample book</h2>
-          <p class="text-gray-900 mt-4">
+      <div class="my-16 flex space-x-8">
+        <div class="">
+          <img
+            class="rounded-lg shadow-lg mb-4"
+            src="http://amirmasoud.me/wp-content/uploads/2020/06/x43190966._SX318_.jpg.pagespeed.ic.-8klxNR3cy.webp"
+          />
+          <div class="mb-8">
+            <span
+              class="px-2 py-1 rounded-md text-xs uppercase bg-gray-100 text-gray-400 border-gray-100 hover:bg-blue-100 hover:text-blue-400 hover:border-blue-100 shadow-xs border"
+              >web</span
+            >
+          </div>
+          <h2 class="font-bold text-base">A sample book</h2>
+          <p class="text-gray-500 mt-4">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
             asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
             enim unde quas impedit aspernatur consequatur quis quibusdam
@@ -91,9 +101,19 @@
           </p>
         </div>
 
-        <div class="uppercase w-1/5">
-          <h2 class="font-bold text-sm">A sample book</h2>
-          <p class="text-gray-900 mt-4">
+        <div class="">
+          <img
+            class="rounded-lg shadow-lg mb-4"
+            src="http://amirmasoud.me/wp-content/uploads/2020/06/x43190966._SX318_.jpg.pagespeed.ic.-8klxNR3cy.webp"
+          />
+          <div class="mb-8">
+            <span
+              class="px-2 py-1 rounded-md text-xs uppercase bg-gray-100 text-gray-400 border-gray-100 hover:bg-blue-100 hover:text-blue-400 hover:border-blue-100 shadow-xs border"
+              >web</span
+            >
+          </div>
+          <h2 class="font-bold text-base">A sample book</h2>
+          <p class="text-gray-500 mt-4">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
             asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
             enim unde quas impedit aspernatur consequatur quis quibusdam
@@ -101,29 +121,19 @@
           </p>
         </div>
 
-        <div class="uppercase w-1/5">
-          <h2 class="font-bold text-sm">A sample book</h2>
-          <p class="text-gray-900 mt-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
-            asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
-            enim unde quas impedit aspernatur consequatur quis quibusdam
-            dolorum, hic nobis nihil.
-          </p>
-        </div>
-
-        <div class="uppercase w-1/5">
-          <h2 class="font-bold text-sm">A sample book</h2>
-          <p class="text-gray-900 mt-4">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
-            asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
-            enim unde quas impedit aspernatur consequatur quis quibusdam
-            dolorum, hic nobis nihil.
-          </p>
-        </div>
-
-        <div class="uppercase w-1/5">
-          <h2 class="font-bold text-sm">A sample book</h2>
-          <p class="text-gray-900 mt-4">
+        <div class="">
+          <img
+            class="rounded-lg shadow-lg mb-4"
+            src="http://amirmasoud.me/wp-content/uploads/2020/06/x43190966._SX318_.jpg.pagespeed.ic.-8klxNR3cy.webp"
+          />
+          <div class="mb-8">
+            <span
+              class="px-2 py-1 rounded-md text-xs uppercase bg-gray-100 text-gray-400 border-gray-100 hover:bg-blue-100 hover:text-blue-400 hover:border-blue-100 shadow-xs border"
+              >web</span
+            >
+          </div>
+          <h2 class="font-bold text-base">A sample book</h2>
+          <p class="text-gray-500 mt-4">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
             asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
             enim unde quas impedit aspernatur consequatur quis quibusdam
