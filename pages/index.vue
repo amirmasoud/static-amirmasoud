75a945a32452c1c06f3d06cd8cab51c78a6bf4a2
Amirmasoud Sheydaei <amirmasood33@gmail.com>
2021-02-08 23:20:39 +0330

    About section

--- pages/index.vue
+++ pages/index.vue
@@ -201,6 +201,25 @@
         </div>
       </div>
     </section>
+
+    <section id="about" class="container mx-auto my-16 px-4 sm:px-16">
+      <h1 class="text-gray-900 font-bold text-3xl">About</h1>
+      <span class="text-blue-500">______</span>
+      <ul class="mt-16">
+        <li class="capitalize mb-16">
+          A sample post <span class="text-gray-900">&rarr;</span>
+        </li>
+        <li class="capitalize mb-16">
+          A sample post <span class="text-gray-900">&rarr;</span>
+        </li>
+        <li class="capitalize mb-16">
+          A sample post <span class="text-gray-900">&rarr;</span>
+        </li>
+        <li class="capitalize mb-16">
+          A sample post <span class="text-gray-900">&rarr;</span>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
