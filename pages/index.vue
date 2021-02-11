036dda5896b90cbe3634aa3b909c0b81a34df2c0
Amirmasoud Sheydaei <amirmasood33@gmail.com>
2021-02-11 23:24:17 +0330

    Fix navbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="bg-gray-50">
-    <nav
-      class="container px-8 sm:px-0 mx-auto flex justify-evenly flex-col sm:flex-row py-8 border-b border-gray-300 text-gray-700 uppercase font-bold text-sm"
-    >
-      <a class="" href="#">Amirmasoud Sheydaei</a>
-      <span class="hidden sm:inline text-gray-200">|</span>
-      <a class="" href="#">Blog</a>
-      <a class="" href="#">Project</a>
-      <a class="" href="#">Books</a>
-      <a class="" href="#">Arts</a>
-      <a class="" href="#">About</a>
-    </nav>
     <section
       id="hello"
       class="container mx-auto px-4 sm:px-16 -mt-20 flex min-h-screen justify-items-center justify-center items-center"
