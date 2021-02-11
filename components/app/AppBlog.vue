beea821a2cf21a0175ff55a1ab5c248b9fa312af
Amirmasoud Sheydaei <amirmasood33@gmail.com>
2021-02-11 23:27:43 +0330

    Refactor blog section

---
+++ components/app/AppBlog.vue
@@ -0,0 +1,20 @@
+<template>
+  <section id="blog" class="container mx-auto my-16 px-4 sm:px-16">
+    <h1 class="text-gray-900 font-bold text-3xl">Blog</h1>
+    <span class="text-blue-500">______</span>
+    <ul class="mt-16">
+      <li class="capitalize mb-16">
+        A sample post <span class="text-gray-900">&rarr;</span>
+      </li>
+      <li class="capitalize mb-16">
+        A sample post <span class="text-gray-900">&rarr;</span>
+      </li>
+      <li class="capitalize mb-16">
+        A sample post <span class="text-gray-900">&rarr;</span>
+      </li>
+      <li class="capitalize mb-16">
+        A sample post <span class="text-gray-900">&rarr;</span>
+      </li>
+    </ul>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,7 @@
   <div class="bg-gray-50">
     <app-hello>Hello! My name is Amirmasood</app-hello>
 
-    <section id="blog" class="container mx-auto my-16 px-4 sm:px-16">
-      <h1 class="text-gray-900 font-bold text-3xl">Blog</h1>
-      <span class="text-blue-500">______</span>
-      <ul class="mt-16">
-        <li class="capitalize mb-16">
-          A sample post <span class="text-gray-900">&rarr;</span>
-        </li>
-        <li class="capitalize mb-16">
-          A sample post <span class="text-gray-900">&rarr;</span>
-        </li>
-        <li class="capitalize mb-16">
-          A sample post <span class="text-gray-900">&rarr;</span>
-        </li>
-        <li class="capitalize mb-16">
-          A sample post <span class="text-gray-900">&rarr;</span>
-        </li>
-      </ul>
-    </section>
+    <app-blog />
 
     <section id="about" class="container mx-auto mt-64 px-4 sm:px-16">
       <h1 class="text-gray-900 font-bold text-3xl">About</h1>
