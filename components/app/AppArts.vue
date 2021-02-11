c58b9b53dd7c5ec44ff5873f5246f619c5916a8b
Amirmasoud Sheydaei <amirmasood33@gmail.com>
2021-02-11 23:32:10 +0330

    Refactor other sections

---
+++ components/app/AppArts.vue
@@ -0,0 +1,59 @@
+<template>
+  <section id="arts">
+    <div class="container mx-auto mt-64 mb-8 px-4 sm:px-16">
+      <h1 class="text-gray-900 font-bold text-3xl">Arts</h1>
+      <span class="text-blue-500">______</span>
+    </div>
+    <div class="magic-grid mx-auto mb-64 mt-16">
+      <div
+        v-for="(img, idx) in Array(1)"
+        :key="idx"
+        class="flex justify-center items-center w-64"
+      >
+        <img
+          class="rounded-md shadow-md"
+          :src="`https://source.unsplash.com/random/${idx + 1}`"
+        />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import MagicGrid from 'magic-grid'
+
+export default {
+  data() {
+    return {
+      magicGrid: null,
+      loadingGrid: true,
+    }
+  },
+  mounted() {
+    const magicGrid = new MagicGrid({
+      container: '.magic-grid',
+      animate: true,
+      gutter: 20,
+      items: 1,
+      useMin: true,
+      maxColumns: 6,
+    })
+
+    magicGrid.listen()
+
+    Promise.all(
+      Array.from(document.images)
+        .filter((img) => !img.complete)
+        .map(
+          (img) =>
+            new Promise((resolve) => {
+              img.onload = img.onerror = resolve
+            })
+        )
+    ).then(() => {
+      magicGrid.positionItems()
+      this.loadingGrid = false
+    })
+  },
+}
+</script>
---
+++ components/app/AppProjects.vue
@@ -0,0 +1,48 @@
+<template>
+  <section id="projects" class="container mx-auto mt-64 px-4 sm:px-16">
+    <h1 class="text-gray-900 font-bold text-3xl">Projects</h1>
+    <span class="text-blue-500">______</span>
+    <ul class="mt-16">
+      <li class="uppercase mb-16">
+        <h2 class="font-bold text-sm">A sample post</h2>
+        <span class="block text-gray-600 font-bold text-xs">2018 - 2020</span>
+        <p class="text-gray-900 mt-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
+          asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
+          enim unde quas impedit aspernatur consequatur quis quibusdam dolorum,
+          hic nobis nihil.
+        </p>
+      </li>
+      <li class="uppercase mb-16">
+        <h2 class="font-bold text-sm">A sample post</h2>
+        <span class="block text-gray-600 font-bold text-xs">2018 - 2020</span>
+        <p class="text-gray-900 mt-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
+          asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
+          enim unde quas impedit aspernatur consequatur quis quibusdam dolorum,
+          hic nobis nihil.
+        </p>
+      </li>
+      <li class="uppercase mb-16">
+        <h2 class="font-bold text-sm">A sample post</h2>
+        <span class="block text-gray-600 font-bold text-xs">2018 - 2020</span>
+        <p class="text-gray-900 mt-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
+          asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
+          enim unde quas impedit aspernatur consequatur quis quibusdam dolorum,
+          hic nobis nihil.
+        </p>
+      </li>
+      <li class="uppercase mb-16">
+        <h2 class="font-bold text-sm">A sample post</h2>
+        <span class="block text-gray-600 font-bold text-xs">2018 - 2020</span>
+        <p class="text-gray-900 mt-4">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, modi
+          asperiores. Quos id aspernatur nesciunt dolorum voluptas? Deleniti
+          enim unde quas impedit aspernatur consequatur quis quibusdam dolorum,
+          hic nobis nihil.
+        </p>
+      </li>
+    </ul>
+  </section>
+</template>
