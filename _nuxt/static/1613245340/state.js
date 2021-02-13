37c8a64650c4118f13e76d7138d10d7f7d1fd630
amirmasoud <amirmasoud@users.noreply.github.com>
2021-02-13 20:00:02 +0000

    deploy: 854510c110547c1d07b4208fbece4df549196785

--- _nuxt/static/1613245340/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1613245340",layout:"default",error:null,serverRendered:a,routePath:"\u002F",config:{content:{dbHash:"b7506875"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
