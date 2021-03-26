15b1b073ba29d727b28955ec7af2448b35c2d138
amirmasoud <amirmasoud@users.noreply.github.com>
2021-03-26 17:37:09 +0000

    deploy: 291e54f8d137f76e623117f88b9a618a95660fa0

---
+++ _nuxt/static/1616780227/blog/7-create-vscode-extension-from-scratch-to-testing-documentation-and-publishing/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616780227",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F7-create-vscode-extension-from-scratch-to-testing-documentation-and-publishing",config:{content:{dbHash:"c8cf1656"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
