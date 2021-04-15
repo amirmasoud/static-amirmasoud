2df489ca0d643dc889308f33d3d7614e55f94119
amirmasoud <amirmasoud@users.noreply.github.com>
2021-04-15 18:27:44 +0000

    deploy: 1e1068630297cfe1c60de42d1e3b641fe06e5e4d

---
+++ _nuxt/static/1618511261/blog/6-installing-mailcatcher-on-linux/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618511261",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F6-installing-mailcatcher-on-linux",config:{content:{dbHash:"fb7d17e2"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
