557de33d2cdf226d7e6bc4271b25037c9e159e27
Amirmasoud <amirmasood33@gmail.com>
2021-04-15 22:51:18 +0430

    Projects WIP

---
+++ content/projects/coir.md
@@ -0,0 +1,7 @@
+---
+from: 2020
+to: 2020
+title: Coir
+---
+
+A docker based project for WordPress theme and plugin development utilizing MariaDB and NGINX.
--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,7 @@ export default {
   async asyncData({ $content }) {
     const hello = await $content('hello-welcome').fetch()
     const about = await $content('about').sortBy('slug', 'desc').fetch()
-    const projects = await $content('projects').fetch()
+    const projects = await $content('projects').sortBy('to', 'desc').fetch()
     const blog = await $content('blog').sortBy('createdAt', 'desc').fetch()
     const books = await $content('books').fetch()
     const connects = await $content('connects').fetch()
