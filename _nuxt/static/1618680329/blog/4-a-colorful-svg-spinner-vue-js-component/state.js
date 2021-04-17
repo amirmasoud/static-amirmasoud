fad05f4170488e1e5837b81634b6d26902d1f396
amirmasoud <amirmasoud@users.noreply.github.com>
2021-04-17 17:38:34 +0000

    deploy: f97478d9b7128508e4eeb36f6956b378acc74ace

--- _nuxt/static/1618680329/blog/4-a-colorful-svg-spinner-vue-js-component/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618680329",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F4-a-colorful-svg-spinner-vue-js-component",config:{content:{dbHash:"5384a6ee"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
--- _nuxt/static/1618680329/blog/6-installing-mailcatcher-on-linux/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618680329",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F6-installing-mailcatcher-on-linux",config:{content:{dbHash:"5384a6ee"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
