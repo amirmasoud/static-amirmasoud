90f3b4b07653f41581e2787314e92626fed4bfc7
amirmasoud <amirmasoud@users.noreply.github.com>
2021-04-17 17:04:40 +0000

    deploy: 7f5fef9ba9a6f07142803f8ad0a4fea52da74cc3

---
+++ _nuxt/static/1618679078/blog/3-start-mailcatcher-on-a-virtual-machine/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618679078",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F3-start-mailcatcher-on-a-virtual-machine",config:{content:{dbHash:"1e3b7776"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
