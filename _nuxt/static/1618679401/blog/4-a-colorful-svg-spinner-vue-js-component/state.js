cdbc114c6c2343e3147b99633c85b73a270d32ee
amirmasoud <amirmasoud@users.noreply.github.com>
2021-04-17 17:15:32 +0000

    deploy: 7c72b9edfe98d0d77a321b455ce65227877e49cb

--- _nuxt/static/1618679401/blog/4-a-colorful-svg-spinner-vue-js-component/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618679401",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F4-a-colorful-svg-spinner-vue-js-component",config:{content:{dbHash:"c41d69b2"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
