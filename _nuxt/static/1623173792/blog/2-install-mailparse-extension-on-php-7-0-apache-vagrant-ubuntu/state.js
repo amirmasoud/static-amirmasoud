67aea4d08950539e694858957be943d8661d87f0
amirmasoud <amirmasoud@users.noreply.github.com>
2021-06-08 17:36:34 +0000

    deploy: b8729752b9a1d70abdda7f11ce3d4841a9bf880a

---
+++ _nuxt/static/1623173792/blog/2-install-mailparse-extension-on-php-7-0-apache-vagrant-ubuntu/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623173792",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F2-install-mailparse-extension-on-php-7-0-apache-vagrant-ubuntu",config:{content:{dbHash:"e1346519"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
