3712796b8189c4cf6f88e56c7255ac2c3f98912f
amirmasoud <amirmasoud@users.noreply.github.com>
2021-06-27 19:16:53 +0000

    deploy: eef934283edb94cb892eb34c9904f4e21ddde8f5

---
+++ _nuxt/static/1624821411/blog/5-consume-and-expose-api-laravel-possible-solutions/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624821411",layout:"default",error:null,serverRendered:a,routePath:"\u002Fblog\u002F5-consume-and-expose-api-laravel-possible-solutions",config:{content:{dbHash:"efdfb9b9"}},colorMode:{preference:b,value:b,unknown:a,forced:false}}}(true,"system"));
